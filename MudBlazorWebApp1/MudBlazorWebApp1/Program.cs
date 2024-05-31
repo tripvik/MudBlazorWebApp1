352ae3bdc223a62f684478f9d4c226a6509c6ad9
Vikash Tripathi <tripvik@gmail.com>
2024-05-31 19:07:41 +0530

    Add project files.

---
+++ MudBlazorWebApp1/MudBlazorWebApp1/Program.cs
@@ -0,0 +1,35 @@
+using MudBlazor.Services;
+using MudBlazorWebApp1.Client.Pages;
+using MudBlazorWebApp1.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddMudServices();
+
+var app = builder.Build();
+
+app.Urls.Add("http://0.0.0.0:8080");
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Counter).Assembly);
+
+app.Run();
